c84e1795b0188f9d7c98fdb228dbff19c4a2b895
UladzimirKurchych <kurchich.v@gmail.com>
2022-11-30 09:27:42 +0100

    Update script.js

---
+++ HTML-CSS_3/js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('.slider').slick({
+nextArrow: '<button class="slick-arrow slick-next"><img src="../HTML-CSS_3/images/header/next.svg.svg" alt="prev-next"></button>',
+prevArrow: '<button class="slick-arrow slick-prev"><img src="../HTML-CSS_3/images/header/prev.svg.svg" alt="prev-next"></button>'
+
+	});
+
+
+});
+
+
+
